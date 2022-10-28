fa848c38271905868227168fbb0584f9518be0ef
ngigordon <ngigordon4@gmail.com>
2022-10-28 21:51:00 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState([]);
+  // useEffect(() => {}, [third]);
+
+  return (
+    <div className="App">
+      <h1>helloworld</h1>
+    </div>
+  );
 }
 
 export default App;
